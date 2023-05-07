c66adfe2b58c144714cb20fdd37f9eaad47a2b23
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-05-07 11:18:31 +0400

    proejct init

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
 
 
 
-app.get('',() => {
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded())
 
+//yıllık gelir
+//ev sahipliği
+//iş süresi
+//Kredi amacı
+//kredi miktarı
+
+
+
+
+app.get('/',(req,res) => {
+    res.send('Hello World');
 })
 
 
-app.post('/credit',() => {
-    
+app.post('/credit',(req,res) => {
+
+    res.send('Hello World');
 })
 
 
